212cf8052c0b035eb257443c3eb66e6d18388779
sanaullah525 <sanasanaullah524@gmail.com>
2026-08-13 00:50:08 +0500

    printing the hash

--- hash-gen.py
+++ hash-gen.py
@@ -7,3 +7,6 @@ password = getpass.getpass("Enter your password: ")
 # Generate the SHA-256 hash (64-character hexadecimal string)
 password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
+
+print("\nSHA-256 Hash:")
+print(password_hash)
