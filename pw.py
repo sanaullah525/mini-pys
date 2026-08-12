f9180a756e4eb23564adc74d8e9dcba8149cb83c
sanaullah525 <sanasanaullah524@gmail.com>
2026-08-12 23:56:06 +0500

    line spacing.

--- pw.py
+++ pw.py
@@ -11,4 +11,5 @@ if hashlib.sha256(entered.encode()).hexdigest() != MASTER_HASH:
 
 PASSWORDS= {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
-            'luggage': '12345'} 
+            'luggage': '12345'}
+
