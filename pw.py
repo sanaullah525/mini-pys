bfc94d6aa48e5de91bd00f255bd9c1e1e5671503
sanaullah525 <sanasanaullah524@gmail.com>
2026-08-12 23:56:39 +0500

    if-statement for invalid entry in account.

--- pw.py
+++ pw.py
@@ -13,3 +13,6 @@ PASSWORDS= {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
             'luggage': '12345'}
 
+if len(sys.argv) < 2:
+  print('Usage: python pw.py [account] - copy account password')
+  sys.exit()
